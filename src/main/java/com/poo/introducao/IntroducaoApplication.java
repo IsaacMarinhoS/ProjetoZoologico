130ecd233f000724ecd7077fcc94f070ddd4d5b4
Isaac Marinho <isaacrosemback0@gmail.com>
2024-07-09 11:24:58 -0300

    inicialização do projeto zoologico, criação de classes

---
+++ src/main/java/com/poo/introducao/IntroducaoApplication.java
@@ -0,0 +1,15 @@
+package com.poo.introducao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroducaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroducaoApplication.class, args);
+
+		
+	}
+
+}
